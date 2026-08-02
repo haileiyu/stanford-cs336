1e16865d1ef88fffc223a1ec793dfa34714f19bb
haileiyu <haileiyu.hly@gmail.com>
2026-08-02 15:46:18 -0400

    added a cache for the sins and coses

--- assignment1-basics/cs336_basics/rope.py
+++ assignment1-basics/cs336_basics/rope.py
@@ -1,26 +1,34 @@
 import torch
 from torch import Tensor, nn
-from einops import einsum
 
 
 class RotaryPositionalEmbedding(nn.Module):
+    cos_cached: Tensor
+    sin_cached: Tensor
+
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
         super().__init__()
         self.theta = theta
         self.d_k = d_k
         self.max_seq_len = max_seq_len
         self.device = device
+        # let's cache the sins and coses
+        t = self.create_theta(torch.arange(max_seq_len, device=device))
+        c, s = torch.cos(t), torch.sin(t)
+        self.register_buffer("cos_cached", c, persistent=False)
+        self.register_buffer("sin_cached", s, persistent=False)
 
     def forward(self, q: Tensor, token_positions: Tensor) -> Tensor:
         """input shapes: q: Float[Tensor, " ... sequence_length d_k"]
         token_positions: Int[Tensor, " ... sequence_length"]"""
-        thetas = self.create_theta(token_positions)
-        c, s = torch.cos(thetas), torch.sin(thetas)
+        c = self.cos_cached[token_positions]
+        s = self.sin_cached[token_positions]
         q1, q2 = q[..., 0::2], q[..., 1::2]
         r = torch.stack([c * q1 - s * q2, s * q1 + c * q2], dim=-1).flatten(-2)
         return r
 
     def create_theta(self, token_positions: Tensor):
-        k = (torch.arange(self.d_k / 2)) * 2 / self.d_k
+        # d_k / 2 is a float. use // to avoid potential landmines in the future.
+        k = (torch.arange(self.d_k // 2, device=self.device)) * 2 / self.d_k
         # [:, None] adds a trailing dimension for the broadcast division to work
         return token_positions[:, None] / torch.pow(self.theta, k)
