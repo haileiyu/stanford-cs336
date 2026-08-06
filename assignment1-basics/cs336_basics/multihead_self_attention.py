21173b7e4fb2d9fc120f676dc6c5b2cebb5992aa
haileiyu <haileiyu.hly@gmail.com>
2026-08-05 20:56:48 -0400

    multi head attention pass!!!

---
+++ assignment1-basics/cs336_basics/multihead_self_attention.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn, Tensor
+from jaxtyping import Float
+import einops
+from einops import einsum
+from cs336_basics.attention import scaled_dot_product_attention
+
+
+class MultiheadSelfAttention(nn.Module):
+    def __init__(self, d_model: int, num_heads):
+        super().__init__()
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+
+    def forward(
+        self,
+        q_proj_weight: Float[Tensor, " d_model d_model"],
+        k_proj_weight: Float[Tensor, " d_model d_model"],
+        v_proj_weight: Float[Tensor, " d_model d_model"],
+        o_proj_weight: Float[Tensor, " d_model d_model"],
+        in_features: Float[Tensor, " ... sequence_length d_model"],
+    ) -> Float[Tensor, " ... sequence_length d_model"]:
+        q_rearranged = einops.rearrange(q_proj_weight, "(a1 a2) b -> a1 a2 b", a1=self.num_heads)
+        k_rearranged = einops.rearrange(k_proj_weight, "(a1 a2) b -> a1 a2 b", a1=self.num_heads)
+        v_rearranged = einops.rearrange(v_proj_weight, "(a1 a2) b -> a1 a2 b", a1=self.num_heads)
+
+        # let's do multi head now
+        wqx = einsum(q_rearranged, in_features, "num_heads d_head d_model, ... seq d_model -> ... num_heads seq d_head")
+        wkx = einsum(k_rearranged, in_features, "num_heads d_head d_model, ... seq d_model -> ... num_heads seq d_head")
+        wvx = einsum(v_rearranged, in_features, "num_heads d_head d_model, ... seq d_model -> ... num_heads seq d_head")
+        d_query = wqx.shape[-2]
+        d_key = wkx.shape[-2]
+        mask = torch.tril(torch.ones(d_query, d_key)).bool()
+        a = scaled_dot_product_attention(wqx, wkx, wvx, mask)  # ... num_heads seq d_head
+        # collapse
+        a_concat = einops.rearrange(a, "... num_heads seq d_head -> ... seq (num_heads d_head)")  # ... seq d_model
+        return einsum(o_proj_weight, a_concat, "a b, ... seq b -> ... seq a")
--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -18,6 +18,7 @@ from cs336_basics.swiglu import SwiGLU, silu
 from cs336_basics.rope import RotaryPositionalEmbedding
 from cs336_basics.softmax import SoftMax
 from cs336_basics.attention import scaled_dot_product_attention
+from cs336_basics.multihead_self_attention import MultiheadSelfAttention
 
 
 def run_linear(
@@ -152,7 +153,8 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    m = MultiheadSelfAttention(d_model, num_heads)
+    return m(q_proj_weight, k_proj_weight, v_proj_weight, o_proj_weight, in_features)
 
 
 def run_multihead_self_attention_with_rope(
