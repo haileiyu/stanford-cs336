902c15dd4a37b55b959531412f820b6a648bce6c
haileiyu <haileiyu.hly@gmail.com>
2026-08-10 22:02:03 -0400

    learning rate schedule

--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -22,6 +22,7 @@ from cs336_basics.multihead_self_attention import MultiheadSelfAttention
 from cs336_basics.transformer import TransformerBlock, TransformerLM
 from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.adamw import AdamW
+from cs336_basics.learning_rate_schedule import lr_cosine_schedule
 
 
 def run_linear(
@@ -522,7 +523,7 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    return lr_cosine_schedule(it, max_learning_rate, min_learning_rate, warmup_iters, cosine_cycle_iters)
 
 
 def run_save_checkpoint(
