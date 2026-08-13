0575e0058090964a7c85390aa908ea0751ca03c6
haileiyu <haileiyu.hly@gmail.com>
2026-08-12 22:07:07 -0400

    checkpoint

--- assignment1-basics/cs336_basics/data_loading.py
+++ assignment1-basics/cs336_basics/data_loading.py
@@ -3,6 +3,14 @@ import numpy.typing as npt
 import numpy
 import torch
 
+import os
+from typing import IO, BinaryIO
+
+
+MODEL_KEY = "model"
+OPTIMIZER_KEY = "optimizer"
+ITERATION_KEY = "iteration"
+
 
 def get_batch(
     dataset: npt.NDArray, batch_size: int, context_length: int, device: str
@@ -38,3 +46,52 @@ def get_batch(
     i = torch.from_numpy(numpy.stack(inputs)).to(device)
     e = torch.from_numpy(numpy.stack(expects)).to(device)
     return tuple[torch.Tensor, torch.Tensor]([i, e])
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | BinaryIO | IO[bytes],
+):
+    """
+    Given a model, optimizer, and an iteration number, serialize them to disk.
+
+    Args:
+        model (torch.nn.Module): Serialize the state of this model.
+        optimizer (torch.optim.Optimizer): Serialize the state of this optimizer.
+        iteration (int): Serialize this value, which represents the number of training iterations
+            we've completed.
+        out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
+    """
+    # get the parameters, put into a map, and serialize via pickle.
+    data = {}
+    data[ITERATION_KEY] = iteration
+    data[MODEL_KEY] = model.state_dict()
+    data[OPTIMIZER_KEY] = optimizer.state_dict()
+    torch.save(data, out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | BinaryIO | IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    """
+    Given a serialized checkpoint (path or file-like object), restore the
+    serialized state to the given model and optimizer.
+    Return the number of iterations that we previously serialized in
+    the checkpoint.
+
+    Args:
+        src (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialized checkpoint.
+        model (torch.nn.Module): Restore the state of this model.
+        optimizer (torch.optim.Optimizer): Restore the state of this optimizer.
+    Returns:
+        int: the previously-serialized number of iterations.
+    """
+
+    data = torch.load(src)
+    model.load_state_dict(data[MODEL_KEY])
+    optimizer.load_state_dict(data[OPTIMIZER_KEY])
+    return data[ITERATION_KEY]
