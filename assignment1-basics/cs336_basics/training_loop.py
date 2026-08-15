7a378ab6052e19681104271ea6c76d291ad676ec
haileiyu <haileiyu.hly@gmail.com>
2026-08-15 11:56:34 -0400

    training loop with checkpointing

--- assignment1-basics/cs336_basics/training_loop.py
+++ assignment1-basics/cs336_basics/training_loop.py
@@ -1,6 +1,4 @@
-import torch
 import einops
-from einops import einsum
 import numpy
 from cs336_basics.adamw import AdamW
 from cs336_basics.transformer import TransformerLM
@@ -8,6 +6,7 @@ from cs336_basics.data_loading import get_batch
 from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.gradient_clipping import gradient_clipping
 from cs336_basics.learning_rate_schedule import lr_cosine_schedule
+from cs336_basics.data_loading import save_checkpoint, load_checkpoint
 
 
 batch_size = 32
@@ -29,13 +28,16 @@ min_lr = 1e-4
 warmup_iters = 50
 cosine_cycle_iters = num_iterations
 
+checkpoint_iters = 10
+checkpoint_file = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_checkpoint"
+
 device = "mps"
-path = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_ids.npy"
+training_data = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_ids.npy"
 
 
-def training_loop(num_iterations: int):
+def training_loop(num_iterations: int, should_load_checkpoint: bool = False):
 
-    dataset = numpy.load(path, mmap_mode="r")
+    dataset = numpy.load(training_data, mmap_mode="r")
 
     tlm = TransformerLM(
         vocab_size=vocab_size,
@@ -46,9 +48,16 @@ def training_loop(num_iterations: int):
         d_ff=d_ff,
         rope_theta=rope_theta,
     ).to(device)
+
     optimizer = AdamW(params=tlm.parameters())
-    for t in range(num_iterations):
-        lr = lr_cosine_schedule(t, max_lr, min_lr, warmup_iters, cosine_cycle_iters)
+
+    checkpointed_iter = -1  # since we need to +1 later
+    if should_load_checkpoint:
+        checkpointed_iter = load_checkpoint(checkpoint_file, tlm, optimizer)
+        print("loaded checkpoint at iteration", checkpointed_iter)
+
+    for iter in range(checkpointed_iter + 1, num_iterations):
+        lr = lr_cosine_schedule(iter, max_lr, min_lr, warmup_iters, cosine_cycle_iters)
         for g in optimizer.param_groups:
             g["lr"] = lr
 
@@ -58,15 +67,24 @@ def training_loop(num_iterations: int):
         out = tlm(input)
         flattened_out = einops.rearrange(out, "a b c -> (a b) c")
 
-        optimizer.zero_grad()  # Reset the gradients for all learnable parameters.
+        optimizer.zero_grad()  # reset the gradients for all learnable parameters
         loss = cross_entropy(flattened_out, flattened_expected_out)
         print(loss.item())
-        loss.backward()  # Run backward pass, which computes gradients.
+        loss.backward()  # run backward pass, which computes gradients
 
         gradient_clipping(tlm.parameters(), max_l2_norm)
 
-        optimizer.step()  # Run optimizer step.
+        optimizer.step()
+
+        # check point
+        if iter % checkpoint_iters == 0:
+            print("saving checkpoint at iteration", iter)
+            save_checkpoint(tlm, optimizer, iter, checkpoint_file)
+
+    # final save
+    print("finally, saving the training")
+    save_checkpoint(tlm, optimizer, num_iterations - 1, checkpoint_file)
 
 
 if __name__ == "__main__":
-    training_loop(num_iterations)
+    training_loop(num_iterations, False)
