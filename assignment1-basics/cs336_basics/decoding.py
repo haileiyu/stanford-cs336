7dce81a1579b5d773450df08dedcb1689c6e0c72
haileiyu <haileiyu.hly@gmail.com>
2026-08-16 12:04:38 -0400

    now the decoder can produce continual text

--- assignment1-basics/cs336_basics/decoding.py
+++ assignment1-basics/cs336_basics/decoding.py
@@ -15,6 +15,9 @@ from cs336_basics.data_loading import load_checkpoint
 import torch
 
 
+
+endoftext_str = "<|endoftext|>"
+
 class Decoder:
     def __init__(self):
         self.tlm = TransformerLM(
@@ -33,17 +36,19 @@ class Decoder:
 
 
 if __name__ == "__main__":
-    input_text = "a cat is"
+    prompt = "a fat cat is"
 
     # tokenize
     vocab_path = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_vocab.pkl"
     merges_path = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_merges.pkl"
-    tokenizer = Tokenizer.from_files(vocab_path, merges_path, ["<|endoftext|>"])
+    tokenizer = Tokenizer.from_files(vocab_path, merges_path, [endoftext_str])
 
     checkpoint_path = "/Users/admin/cs336/assignment1-basics/tiny_stories_10000_checkpoint.cpt"
 
     # encoding
-    encoded = tokenizer.encode(input_text)
+    token_indexes = tokenizer.encode(prompt)
+    
+    endoftext_index = tokenizer.inverse_vocab[endoftext_str.encode("utf-8")]
 
     # forward
     tlm = TransformerLM(
@@ -61,13 +66,29 @@ if __name__ == "__main__":
 
     num_iters = 100
 
-    for i in range(num_iters):
-        encoded_converted = torch.tensor(encoded, dtype=torch.long, device=device).unsqueeze(0)
-        out = tlm(encoded_converted)
+    print(prompt)
 
-        # pick the most likely token, and append to the list, and loop
-        index = out[0, -1, :].argmax().item()
-        encoded.append(index)
+    # torch.no_grad would save memory by turning off the grad (for back propagation)
+    with torch.no_grad():
 
-    print(encoded)
-    print(tokenizer.decode(encoded))
+        for i in range(num_iters):
+            # truncate to context_length
+            context_window = token_indexes[-context_length :]
+
+            # convert to a tensor, flatten, and feed into transformer
+            out = tlm(torch.tensor(context_window, dtype=torch.long, device=device).unsqueeze(0))
+
+            # pick the most likely token, and append to the list, and loop
+            index = out[0, -1, :].argmax().item()
+            
+            # break if end of text
+            if index == endoftext_index:
+                print("end")
+                break
+            
+            token_indexes.append(index)
+            print(tokenizer.decode([index]), end="", flush=True)
+
+    print()
+    print(token_indexes)
+    print(tokenizer.decode(token_indexes))
